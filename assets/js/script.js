93ae4f7bb975f02b37e39c5e52f2e0595c2ad718
Harry <Haxmat@users.noreply.github.com>
2017-06-29 07:26:29 +0100

    Changed Colours on Skills

--- assets/js/script.js
+++
@@ -1,8 +0,0 @@
-$(window).on("load", function() {
-  $('#nav').fadeIn(1000).removeClass('hidden');
-  $('#name').fadeIn(2000).removeClass('hidden');
-  $('#subtitle').fadeIn(1700).removeClass('hidden');
-  $('#codeico').fadeIn(3000).removeClass('hidden');
-  $('#main').fadeIn(1500).removeClass('hidden');
-  $('#footer').fadeIn(1600).removeClass('hidden');
-});
