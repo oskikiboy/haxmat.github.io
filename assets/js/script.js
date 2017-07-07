06eb6df434e9502d343a2c3b90f8e43fb422b646
Harry <Haxmat@users.noreply.github.com>
2017-07-07 07:33:16 +0100

    Added Scripts

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('.typed').typed({
+       strings: ["Junior Web Developer"],
+        typeSpeed: 10
+    });
+});
+$(window).scroll(function() {
+    var top = $('#skills').offset().top;
+    if ($(this).scrollTop() >= top) {
+        $('.nav').css({'opacity': 0.5});
+    } else {
+        $('.nav').css({'opacity': 1});
+    }
+});
